42f907a3478d71d5ae729565d02341fe2fa8a438
Alex Napier <alex.r.napier@gmail.com>
2016-12-01 03:46:39 -0600

    commented out javascript to prepare for new implementation of header classes on scroll

--- js/app.js
+++ js/app.js
@@ -5,26 +5,8 @@ var logoText = $('.header__logo--text');
 var subText = $('.header__subtext');
 
 
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 1) {
-    $(header).css({
-      height: '10vh',
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'flex-start'
-    });
-    $(logo).css({
-      height: '3em',
-      width: '3em',
-      marginBottom: '0',
-      marginLeft: '3em'
-    });
-    $(logoText).css({
-      fontSize: '1.2em',
-      marginBottom: '0',
-      marginTop: '0',
-      marginLeft: '1em'
-    });
-    $(subText).remove();
-  }
-});
+// $(window).scroll(function() {
+//   if ($(this).scrollTop() > 1) {
+//     $(subText).remove();
+//   }
+// });
